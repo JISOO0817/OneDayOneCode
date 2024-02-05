9438e47a385b12f2cce87043f9f1631367128664
janzizu <dkadnsk@naver.com>
2024-02-05 22:38:10 +0900

    [Add] Day3 code

--- Day3/BOJ10430.kt
+++ Day3/BOJ10430.kt
@@ -1,4 +1,19 @@
 package Day3
 
-class BOJ10430 {
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val input = StringTokenizer(br.readLine())
+
+    val A = input.nextToken().toInt()
+    val B = input.nextToken().toInt()
+    val C = input.nextToken().toInt()
+
+    println("${(A + B) % C}")
+    println("${((A % C) + (B % C)) % C}")
+    println("${(A * B) % C}")
+    println("${((A % C) * (B % C)) % C}")
 }
