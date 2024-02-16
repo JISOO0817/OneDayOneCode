b2461e909602205136cd16a21e96b444c10f04c7
janzizu <dkadnsk@naver.com>
2024-02-16 16:12:18 +0900

    [Add] Day11

--- Day11/BOJ10872.kt
+++ Day11/BOJ10872.kt
@@ -6,7 +6,6 @@ import java.io.InputStreamReader
 fun main() {
 
     val br=  BufferedReader(InputStreamReader(System.`in`))
-
     val n = br.readLine().toInt()
 
     print(getFactorial(n))
--- Day11/BOJ18108.kt
+++ Day11/BOJ18108.kt
@@ -4,6 +4,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() {
+
     val br = BufferedReader(InputStreamReader(System.`in`))
     val y = br.readLine().toInt()
 
