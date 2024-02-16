1c71cbb1d4b01b86d628b18ab536b5aa3493ccd9
janzizu <dkadnsk@naver.com>
2024-02-16 16:07:08 +0900

    [Add] Day9 & Day10

---
+++ Day11/BOJ2558.kt
@@ -0,0 +1,11 @@
+package Day11
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val a = br.readLine().toInt()
+    val b = br.readLine().toInt()
+    print(a + b)
+}
