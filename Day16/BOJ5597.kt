e8ccaddc349f17891f689753ec68094ee0cc2f79
janzizu <dkadnsk@naver.com>
2024-02-21 21:42:41 +0900

    [Add] Day16

---
+++ Day16/BOJ5597.kt
@@ -0,0 +1,20 @@
+package Day16
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val studentAllList = Array(30) { false }
+    repeat(28) {
+        val index = br.readLine().toInt()
+        studentAllList[index - 1] = true
+    }
+
+    studentAllList.forEachIndexed { index, value ->
+        if (!value) {
+            println(index + 1)
+        }
+    }
+}
